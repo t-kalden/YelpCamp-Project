42c87ca3e35b2c988693a163d606e05aad3d5756
t-kalden <kalden.t@outlook.com>
2022-02-03 10:12:35 -0500

    testing connection to db and routes

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const Campground = require('./models/campground');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("err", console.error.bind(console, "CONNECTION ERROR: "));
+db.once("open", () => {
+    console.log("CONNECTED TO DATABASE");
+})
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req,res) => {
-    res.send("YelpCamp")
+    res.render('home');
+})
+
+app.get('/makecampground', async (req,res) => {
+    const camp = new Campground({title: 'MY BACKYARD TESTING', description: 'testing db'});
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
