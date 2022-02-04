0f0dec83deaec49671c740180adfdaf93805cec3
t-kalden <kalden.t@outlook.com>
2022-02-04 10:50:14 -0500

    Updated seeds folder to include image and description

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async() => {
     
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() *60) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae incidunt ipsam suscipit quas totam provident iste blanditiis culpa impedit, expedita dicta magnam, veniam molestias, magni consectetur aliquam doloremque praesentium eius.",
+            price
         })
         await camp.save();
     }
