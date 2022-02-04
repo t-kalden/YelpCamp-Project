9274818e87e99b19d7e0f0eaa9853c451b3efb7c
t-kalden <kalden.t@outlook.com>
2022-02-04 09:35:57 -0500

    Adding ejs-mate to app.js to refrence for boiler plate functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -12,9 +13,14 @@ db.once("open", () => {
     console.log("CONNECTED TO DATABASE");
 })
 
+
 const app = express();
 app.use(express.urlencoded({ extended : true }));
+//calling method_override
 app.use(methodOverride('_method'));
+//calling ejs-mate for boilerplate 
+app.engine('ejs', ejsMate);
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
