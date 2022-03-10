dfe92fa94b2bc1415b1dde69114fe3141d8e68ca
t-kalden <kalden.t@outlook.com>
2022-03-10 10:50:22 -0500

    Feeding reviews to show page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const { campgroundSchema } = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const Review = require('./models/review');
 
 const mongoose = require('mongoose');
@@ -53,6 +53,14 @@ const validateCampground = (req,res,next) => {
     } else {next()}
 }
 
+const validateReview = (req,res,next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {next()} 
+}
+
 app.post('/campgrounds', validateCampground, catchAsync(async (req,res, next) => {
          // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
         const campground = new Campground(req.body.campground);
@@ -78,7 +86,7 @@ app.delete('/campgrounds/:id', catchAsync( async (req,res) => {
     res.redirect("/campgrounds");
 }))
 
-app.post('/campgrounds/:id/reviews', catchAsync(async(req,res) => {
+app.post('/campgrounds/:id/reviews',validateReview, catchAsync(async(req,res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
@@ -89,7 +97,7 @@ app.post('/campgrounds/:id/reviews', catchAsync(async(req,res) => {
 
 //show page - showing details of campgrounds
 app.get('/campgrounds/:id',  catchAsync( async(req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', {campground});
 }))
 
