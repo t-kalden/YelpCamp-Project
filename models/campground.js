ee252869a36a6d568be344ae8e48f8a27075cbff
t-kalden <kalden.t@outlook.com>
2022-03-10 11:22:18 -0500

    Deleting associated reviews when deleting a campground

--- models/campground.js
+++ models/campground.js
@@ -16,4 +16,15 @@ const campgroundSchema = new Schema({
     ]
 })
 
+
+campgroundSchema.post('findOneAndDelete', async function(doc) {
+    if(doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
 module.exports = mongoose.model('Campground', campgroundSchema);
