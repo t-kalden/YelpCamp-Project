3be10258b063d23b38db3e7f169d69e4b01e9e73
t-kalden <kalden.t@outlook.com>
2022-03-10 10:07:13 -0500

    Debugging reviewsSchema export

--- models/review.js
+++ models/review.js
@@ -6,4 +6,4 @@ const reviewSchema = new Schema({
     rating: Number
 });
 
-module.exports = mongoose.mongoose.model('Campground', CampgroudSchema);
+module.exports = mongoose.model('Review', reviewSchema);
