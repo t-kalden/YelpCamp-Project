3a48568c79456d281b16858ed42f2a439dcae44b
t-kalden <kalden.t@outlook.com>
2022-03-10 10:50:59 -0500

    Debugging to add required functionality to reviews form

--- schemas.js
+++ schemas.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const review = require('./models/review');
 
 
 module.exports.campgroundSchema = Joi.object( {
@@ -8,6 +9,13 @@ module.exports.campgroundSchema = Joi.object( {
             image: Joi.string().required(),
             location: Joi.string().required(),
             description: Joi.string().required(),
-        }
-    ).required()
+        }).required()
 });
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().min(1).max(5).required(),
+        body:Joi.string().required()
+    }).required()
+})
+
