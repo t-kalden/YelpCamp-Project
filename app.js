234b90ac1e9e9f0fb02bff723fceeade3e283eac
t-kalden <kalden.t@outlook.com>
2022-03-11 11:28:49 -0500

    Added cookies

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const session = require('express-session')
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const app = express();
 
@@ -20,13 +22,32 @@ db.once("open", () => {
 app.use(express.urlencoded({ extended : true }));
 //calling method_override
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
 //calling ejs-mate for boilerplate 
 app.engine('ejs', ejsMate);
+const sessionConfig = {
+    secret:'Secret',
+    resave:false,
+    saveUninitalized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + (1000 * 60 * 60 * 24 * 7),
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req,res) => {
     res.render('home');
+})
+
+app.use((req,res,next)=> {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
 })
 
 app.use('/campgrounds', campgrounds);
