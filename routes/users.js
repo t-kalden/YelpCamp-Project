d3e56a2d8df2f03d02a014dfca54ea417f14f13a
t-kalden <kalden.t@outlook.com>
2022-03-13 19:33:04 -0400

    Created login page and added login logic

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const _ = require('passport-local-mongoose');
 const user = require('../models/user');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport')
 
 router.get('/register', (req,res) => {
     res.render('users/register');
@@ -22,11 +24,12 @@ router.post('/register', catchAsync(async(req,res) => {
 }));
 
 router.get('/login', (req,res) => {
-    res.render('user/login');
+    res.render('users/login');
 })
 
-router.post('/login', (req,res) => {
-
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect:'/login'} ),(req,res) => {
+    req.flash('success', 'Welcome Back');
+    res.redirect('/campgrounds');
 })
 
 module.exports = router;
