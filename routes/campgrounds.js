d19292c9ec172cd928a7ac7b941904a65d0808f3
t-kalden <kalden.t@outlook.com>
2022-03-18 10:52:39 -0400

    Adding author to user model

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -29,6 +29,7 @@ router.get('/new', isLoggedIn, (req,res) => {
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req,res, next) => {
          // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
         const campground = new Campground(req.body.campground);
+        campground.author = req.user._id;
         await campground.save();
         req.flash('success', 'Thank you for adding a new campground!')
         res.redirect(`/campgrounds/${campground._id}`);
@@ -60,7 +61,7 @@ router.delete('/:id', isLoggedIn, catchAsync( async (req,res) => {
 
 //show page - showing details of campgrounds
 router.get('/:id', catchAsync( async(req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
     if(!campground) {
         req.flash('error', "Uh-oh, cannot find that campground"); 
         return res.redirect("/campgrounds");
