035beea55a513cedc06eff411a7fb4f16b800980
t-kalden <kalden.t@outlook.com>
2022-03-18 10:06:33 -0400

    Adding logic to keep user signed in when registering

--- routes/users.js
+++ routes/users.js
@@ -10,13 +10,16 @@ router.get('/register', (req,res) => {
     res.render('users/register');
 })
 
-router.post('/register', catchAsync(async(req,res) => {
+router.post('/register', catchAsync(async(req,res,next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username })
         const registeredUser = await User.register(user,password);
-        req.flash('success', 'You have been registered');
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if(err) return next(err);
+            req.flash('success', 'You have been registered');
+            res.redirect('/campgrounds');
+        });
     } catch(e) {
         req.flash('error', e.message);
         res.redirect('/register');
