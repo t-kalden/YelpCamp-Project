53b3e31468c9b6523b859cd441d543c87718cf02
t-kalden <kalden.t@outlook.com>
2022-03-18 10:13:00 -0400

    Adding returnToUrl logic to have user return to desired page after being prompted to login

--- routes/users.js
+++ routes/users.js
@@ -32,7 +32,9 @@ router.get('/login', (req,res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect:'/login'} ),(req,res) => {
     req.flash('success', 'Welcome Back');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 })
 
 router.get('/logout', (req,res) => {
