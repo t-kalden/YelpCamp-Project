d0c07db7118499dfb86b48832d2627c4739cdf4a
t-kalden <kalden.t@outlook.com>
2022-03-19 15:42:49 -0400

    fixed campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,20 +2,17 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-const campgrounds = require('../controller/campgrounds');
 const multer = require('multer');
 const { storage } = require('../cloudinary');
 const upload = multer({ storage });
+const campgrounds = require('../controller/campgrounds');
 
 router.route('/')
     //index page - display ALL campground C(Read)UD
         .get(catchAsync(campgrounds.index)) 
     //create and add new campground to db
-        // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground)) 
-        .post(upload.array('image'), (req,res) => {
-            console.log(req.body, req.files);
-            res.send('Files uploaded');
-        })
+        .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground)) 
+
 
 //route to form to add new campground to database (Create)RUD
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
