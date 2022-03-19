bbd80f8d1d3805e3a7cef178ed2ec98d956a6f15
t-kalden <kalden.t@outlook.com>
2022-03-19 15:42:01 -0400

    fixed campground

--- controller/campgrounds.js
+++ controller/campgrounds.js
@@ -1,59 +1,66 @@
-const Campground = require('../models/campground');
+const Campground = require("../models/campground");
 
 //index controller
-module.exports.index = async (req,res) => {
-    const campgrounds = await Campground.find();
-    res.render('campgrounds/index', {campgrounds});
-}
+module.exports.index = async (req, res) => {
+  const campgrounds = await Campground.find();
+  res.render("campgrounds/index", { campgrounds });
+};
 
 //new campgrounds form controller
-module.exports.renderNewForm = (req,res) => {
-    res.render('campgrounds/new');
-}
-//create new campgrounds 
-module.exports.createCampground = async (req,res, next) => {
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Thank you for adding a new campground!')
-    res.redirect(`/campgrounds/${campground._id}`);
-}
+module.exports.renderNewForm = (req, res) => {
+  res.render("campgrounds/new");
+};
+//create new campgrounds
+module.exports.createCampground = async (req, res, next) => {
+  const campground = new Campground(req.body.campground);
+  campground.images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+  campground.author = req.user._id;
+  await campground.save();
+  console.log(campground);
+  req.flash("success", "Thank you for adding a new campground!");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
 
-//edit campgrounds 
+//edit campgrounds
 module.exports.renderEditForm = async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if(!campground) {
-        req.flash('error', "Uh-oh, cannot find that campground"); 
-        return res.redirect("/campgrounds");
-    }
-    res.render('campgrounds/edit', { campground });
-}
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    req.flash("error", "Uh-oh, cannot find that campground");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/edit", { campground });
+};
 
-//update campgrounds 
-module.exports.updateCampground = async (req,res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-    req.flash('success', 'Campground has been updated!')
-    res.redirect(`/campgrounds/${campground._id}`);
-}
+//update campgrounds
+module.exports.updateCampground = async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  req.flash("success", "Campground has been updated!");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
 
 //delete campgrounds
-module.exports.deleteCampground = async (req,res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Campground has been deleted!')
-    res.redirect("/campgrounds");
-}
+module.exports.deleteCampground = async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  req.flash("success", "Campground has been deleted!");
+  res.redirect("/campgrounds");
+};
 
-//show campgrounds 
-module.exports.showCampground = async(req, res) => {
-    const campground = await Campground.findById(req.params.id)
-        .populate({path: 'reviews', populate: {path: 'author'}})
-        .populate('author');
-    if(!campground) {
-        req.flash('error', "Uh-oh, cannot find that campground"); 
-        return res.redirect("/campgrounds");
-    }
-    res.render('campgrounds/show', { campground });
-}
+//show campgrounds
+module.exports.showCampground = async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("author");
+  if (!campground) {
+    req.flash("error", "Uh-oh, cannot find that campground");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/show", { campground });
+};
