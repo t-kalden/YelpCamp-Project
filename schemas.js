3fb0199cd97925fdf32519809988fb8753d28644
t-kalden <kalden.t@outlook.com>
2022-03-19 15:46:23 -0400

    updaing schema

--- schemas.js
+++ schemas.js
@@ -1,21 +1,19 @@
-const Joi = require('joi');
-const review = require('./models/review');
+const Joi = require("joi");
+const { number } = require("joi");
 
-
-module.exports.campgroundSchema = Joi.object( {
-    campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required(),
-        }).required()
+module.exports.campgroundSchema = Joi.object({
+  campground: Joi.object({
+    title: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    // image: Joi.string().required(),
+    location: Joi.string().required(),
+    description: Joi.string().required(),
+  }).required(),
 });
 
 module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().min(1).max(5).required(),
-        body:Joi.string().required()
-    }).required()
-})
-
+  review: Joi.object({
+    rating: Joi.number().min(1).max(5).required(),
+    body: Joi.string().required(),
+  }).required(),
+});
