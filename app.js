a9f6dbc67cd07048df7edd30ff1e8f8a6d522715
t-kalden <kalden.t@outlook.com>
2022-03-29 19:24:06 -0400

    disregard

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoSanitize = require('express-mongo-sanitize');
+// const helmet = require('helmet');
 
 //routes
 const campgroundsRoutes = require('./routes/campgrounds');
@@ -52,6 +53,52 @@ app.use(methodOverride('_method')); //calling method_override
 app.use(mongoSanitize());
 app.use(session(sessionConfig));
 app.use(flash());
+
+//helmet
+// app.use(helmet());
+// const scriptSrcUrls = [
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://api.mapbox.com/",
+//     "https://kit.fontawesome.com/",
+//     "https://cdnjs.cloudflare.com/",
+//     "https://cdn.jsdelivr.net",
+// ];
+// const styleSrcUrls = [
+//     "https://kit-free.fontawesome.com/",
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.mapbox.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://fonts.googleapis.com/",
+//     "https://use.fontawesome.com/",
+// ];
+// const connectSrcUrls = [
+//     "https://api.mapbox.com/",
+//     "https://a.tiles.mapbox.com/",
+//     "https://b.tiles.mapbox.com/",
+//     "https://events.mapbox.com/",
+// ];
+// const fontSrcUrls = [];
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             defaultSrc: [],
+//             connectSrc: ["'self'", ...connectSrcUrls],
+//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//             workerSrc: ["'self'", "blob:"],
+//             objectSrc: [],
+//             imgSrc: [
+//                 "'self'",
+//                 "blob:",
+//                 "data:",
+//                 "https://res.cloudinary.com/dplz0x0mm/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+//                 "https://images.pexels.com/",
+//             ],
+//             fontSrc: ["'self'", ...fontSrcUrls],
+//         },
+//     })
+// );
 
 //pasport
 app.use(passport.initialize()); // initilizing passport
